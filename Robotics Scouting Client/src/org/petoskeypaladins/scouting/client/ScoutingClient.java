f6a5792d7d88e48ca0622443bcdc503d34a1c602
killjoy64 <kyle.flynn5798@yahoo.com>
2015-01-25 17:22:41 -0500

    Finally Updated Scouting Client Properly

---
+++ Robotics Scouting Client/src/org/petoskeypaladins/scouting/client/ScoutingClient.java
@@ -0,0 +1,43 @@
+package org.petoskeypaladins.scouting.client;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ScoutingClient extends Application {
+
+	private static Stage appStage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			ScoutingClient.appStage = primaryStage;
+			
+			Parent root = FXMLLoader.load(getClass().getResource("xml/Application.fxml"));
+			
+			Scene scene = new Scene(root);
+			
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Scouting Program 2014 - v0.1");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+				
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static Stage getStage() {
+		return appStage;
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
