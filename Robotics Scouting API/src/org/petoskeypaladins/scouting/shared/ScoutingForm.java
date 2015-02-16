edbffd41a6b65a56a955fac09780bfb654850e70
Taylor Brown <readingmaniac24@gmail.com>
2015-02-16 18:13:03 -0500

    Some more changes that involve indexing

--- Robotics Scouting API/src/org/petoskeypaladins/scouting/shared/ScoutingForm.java
+++ Robotics Scouting API/src/org/petoskeypaladins/scouting/shared/ScoutingForm.java
@@ -1,5 +1,10 @@
 package org.petoskeypaladins.scouting.shared;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,18 +19,49 @@ public class ScoutingForm implements Serializable {
 	private HashMap<String, String> textObjectFields;
 	private ArrayList<String> tmp;
 	
+	private File form;
+	
 	public ScoutingForm() {
 		singleObjectFields = new HashMap<String, Object>();
 		textObjectFields = new HashMap<String, String>();
 		tmp = new ArrayList<String>();
 	}
 	
+	public ScoutingForm(String path) {
+		this.form = new File(path);
+		
+		singleObjectFields = new HashMap<String, Object>();
+		textObjectFields = new HashMap<String, String>();
+		tmp = new ArrayList<String>();
+		
+		try {
+			FileReader reader = new FileReader(form);
+			BufferedReader br = new BufferedReader(reader);
+			
+			String line;
+			
+			try {
+				while((line = br.readLine()) != null) {
+					// Initialize objects parameter and put all of the objectFields inside of a hashmap
+					String[] objects = line.split(": ");
+					addObjectField(objects[0], objects[1]);
+				}
+			} catch (IOException e) {
+				System.out.println("API: IO Exception!");
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("API: Could not read file!");
+		}
+		
+	}
+	
 	public void addObjectField(String field, Object o) {
-		singleObjectFields.put(field, o);
+		singleObjectFields.put(field.toLowerCase(), o);
 	}
 	
 	public void addObjectFields(String field, String text) {
-		textObjectFields.put(field, text);
+		textObjectFields.put(field.toLowerCase(), text);
 	}
 	
 	public Object getObjectField(String field) {
--- Robotics Scouting Server/src/org/petoskeypaladins/scouting/server/commands/CommandList.java
+++ Robotics Scouting Server/src/org/petoskeypaladins/scouting/server/commands/CommandList.java
@@ -7,15 +7,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.petoskeypaladins.scouting.server.ServerLog;
 import org.petoskeypaladins.scouting.server.util.Properties;
+import org.petoskeypaladins.scouting.shared.ScoutingForm;
 
 public class CommandList extends Command {
 
 	public CommandList(){
 		setName("list");
-		setUsage("list <field=query> <field>");
+		setUsage("list <field=query> <field> <can-see-duplicates>");
 	}
 
 	@Override
@@ -23,93 +27,41 @@ public class CommandList extends Command {
 		// e.g. list spos 3618
 		if(args.length == 3) {						
 			File f = new File(Properties.FORM_FILE_PATH);
+					
+			ArrayList<File> formFiles = new ArrayList<File>();
+			ScoutingForm tempForm;
 			
 			ArrayList<String> queries = new ArrayList<String>();
 			
-			String queryResult = "NULL";			
-			// Loop through ALL the files in the FORM_FILE_PATH
 			for(File file : f.listFiles()) {
-				if(file.isFile()) {
-					// Fields for fileParam[] array
-					// [0] = Competition
-					// [1] = Round
-					// [2] = Team
-					/*
-					 * Right now it works by asking for the team#, then by searching for the arg[1] based on team number
-					 * What it can't do RIGHT NOW: 
-					 * 	Compare Fields: round_number competition == 
-					 * 
-					 * Maybe it should search by round_number=3618 - searches for round_number where 3618 is in
-					 * 
-					 * list round_number=5 team_number - and it gives the team numbers that were in 5
-					 * list competitoin=tc team_number - and it gives the team numbers that were in tc
-					 * 
-					 * 1 = field=query
-					 * 2 = field to search for
-					 */
-					String[] fileParam = file.getName().replace(".form", "").split("-");
+				formFiles.add(file);
+				try {
+					tempForm = new ScoutingForm(file.getPath());					
 					
+					String dataQuery = args[2].replace("_", " ");
 					String[] query = args[1].split("=");
+					String eQuery = query[0].replace("_", " ");
 					
-					try {
-						FileReader reader = new FileReader(file);
-						BufferedReader br = new BufferedReader(reader);
-						
-						try {
-							String line;
-							while((line = br.readLine()) != null) {
-								String[] info = line.split(": ");
-								String firstField = info[0]; // This is the obectField
-								String secondField = info[1];// This is the result of the obJectField
-								
-								// If the field has a space, then replace it with an underscore
-								if(info[0].contains(" ")) {
-									firstField = info[0].replace(" ", "_");
-								}
-								
-								int curIndex = 0;
-								
-								// Check if the second field equals the first field in the file
-								if(args[2].equalsIgnoreCase(firstField)) {
-									queryResult = secondField;
-									//if(args[2].equalsIgnoreCase("team_number") || args[2].equalsIgnoreCase("competition")) {
-									//e.g. if queries already contains 3618, set it to NULL and don't print it out
-										if(queries.contains(queryResult)) {
-											if(queries.indexOf(queryResult) == -1) {
-												queryResult = "NULL";
-											}
-										} else {
-											queries.add(queryResult);
-										}
-									//}
-								}
-								
-								// Now search for team_number where round_number=5
-									if(query[0].equalsIgnoreCase(firstField)) {		// checks if round_number equals the round_number field in the file so we don't get team_number = 5
-										if(query[1].equalsIgnoreCase(secondField)) {	// checks if the query equals the result of the first field
-											// How do I get the team number?
-											if(!queryResult.equalsIgnoreCase("NULL")) {
-												ServerLog.logInfo("Result of " + args[2] + " where " + query[0] + " = " + query[1] + ": " + queryResult);
-											}
-										}
+					if(tempForm.getObjectField(eQuery) != null) {
+					// We found the query of the first criteria field
+					// Eventually want to getObjectField(team_number) from round_number when it equals 5
+						if(tempForm.getObjectField(eQuery).equals(query[1].replace("_", " "))) {
+							if(args.length == 4) {
+								if(args[3].equalsIgnoreCase("true")) {
+									if(!queries.contains(dataQuery)) {
+										ServerLog.logInfo("Result of " + args[2] + " where " + query[0] + " = " + query[1] + ": " + tempForm.getObjectField(dataQuery));								
+										queries.add(dataQuery);
 									}
+								}
+							} else {
+								ServerLog.logInfo("Result of " + args[2] + " where " + query[0] + " = " + query[1] + ": " + tempForm.getObjectField(dataQuery));
 							}
-							
-						} catch (IOException e) {
-							ServerLog.logError("Could not read files");							
 						}
-					} catch (FileNotFoundException e) {
-						ServerLog.logError("Could not read files");
 					}
 					
+				} catch (Exception e) {;				
 				}
-			}
-			
-			queries.clear();
-			
-		} else {
-			ServerLog.logError(getUsage());
+			}	
 		}
 	}
-	
 }
